e04dc85fa192b33fc2af1484ab21d798726cdf2d
claudio sultanio <claudiosultanio@gmail.com>
2024-01-21 19:22:02 -0300

    Desafio concluído

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,19 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+    public Nokia(string numero,string modelo,string imei,int memoria) : base (numero,modelo,imei,memoria)
+    {
+        
     }
-}
+            
+            // TODO: Sobrescrever o método "InstalarAplicativo"
+            public override void InstalarAplicativo(string maps)
+            {
+                Console.WriteLine($"Instalando Aplicativo {maps} no Nokia...");
+                Console.WriteLine($"{maps} instalado com sucesso!");
+            }
+        } 
+} 
+  
--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+ Smartphone nokia = new Nokia(numero:"1910101010",modelo:"N22",imei:"9876543210",memoria:128);
+ nokia.Ligar();
+ nokia.InstalarAplicativo("maps");
+
+ Console.WriteLine("\n");
+
+ Console.WriteLine("Smartphone Iphone:");
+ Smartphone iphone = new Iphone(numero:"1912341234",modelo:"14PRO",imei:"9876543210",memoria:128);
+ iphone.Ligar();
+ iphone.InstalarAplicativo("waze");
+
+
+
+
